7dec19b097c2337a9f6a592931ed951d0652a198
Hector Torre <hector.torre@globallogic.com>
2024-09-04 16:46:31 +0200

    04092024: Added base project MCUXpresso for KL25Z. Accelerometer and I2C code included

---
+++ MKL25Z4_Project/Debug/source/i2c.d
@@ -0,0 +1,14 @@
+source/i2c.o source/i2c.d: ../source/i2c.c ../source/I2C.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/MKL25Z4.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cm0plus.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cmInstr.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/cmsis_gcc.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cmFunc.h \
+ C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/system_MKL25Z4.h
+../source/I2C.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/MKL25Z4.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cm0plus.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cmInstr.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/cmsis_gcc.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/core_cmFunc.h:
+C:\Users\hector.torre\Documents\MCUXpressoIDE_11.9.0_2144\workspace\MKL25Z4_Project\CMSIS/system_MKL25Z4.h:
